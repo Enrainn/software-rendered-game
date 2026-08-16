f7086cff84cf1f887aa3fcdc04e051cba27fdcd5
Gabriel S. Martins <gabrielm_gamer@hotmail.com>
2026-08-16 15:11:56 -0300

    Add DataDraw chunk schema integration

--- src/chunk.c
+++ src/chunk.c
@@ -2,6 +2,17 @@
 #include <stdlib.h>
 #include <math.h>
 
+#if defined(__has_include)
+#  if __has_include("rcdatabase.h")
+#    include "rcdatabase.h"
+#    define CHUNKS_HAVE_DATADRAW 1
+#  endif
+#endif
+
+#ifndef CHUNKS_HAVE_DATADRAW
+#  define CHUNKS_HAVE_DATADRAW 0
+#endif
+
 static int chunk_compare(const void *a, const void *b) {
     float da = ((const Chunk *)a)->depth_sort_key;
     float db = ((const Chunk *)b)->depth_sort_key;
@@ -25,4 +36,71 @@ ScreenAABB chunk_screen_aabb(const Chunk *chunk) {
     bb.y_max = (int)ceilf(maxf(chunk->verts[0].y,
                                 maxf(chunk->verts[1].y, chunk->verts[2].y)));
     return bb;
+}
+
+
+void chunks_datadraw_start(void) {
+#if CHUNKS_HAVE_DATADRAW
+    rcDatabaseStart();
+#endif
+}
+
+void chunks_datadraw_record_frame(const Chunk *chunks, int chunk_count, uint32_t frame_index) {
+#if CHUNKS_HAVE_DATADRAW
+    rcRootFreeAll();
+    rcChunkFreeAll();
+    rcScreenAABBFreeAll();
+
+    rcRoot root = rcRootAlloc();
+    rcRootSetFrameIndex(root, frame_index);
+
+    for (int i = 0; i < chunk_count; i++) {
+        const Chunk *src = &chunks[i];
+        rcChunk dst = rcChunkAlloc();
+
+        rcChunkSetType(dst, src->type == CHUNK_COLORED ? RC_CHUNK_COLORED : RC_CHUNK_TEXTURED);
+        rcChunkSetDepthSortKey(dst, src->depth_sort_key);
+        rcChunkSetX0(dst, src->verts[0].x);
+        rcChunkSetY0(dst, src->verts[0].y);
+        rcChunkSetZ0(dst, src->verts[0].z);
+        rcChunkSetInvW0(dst, src->verts[0].inv_w);
+        rcChunkSetX1(dst, src->verts[1].x);
+        rcChunkSetY1(dst, src->verts[1].y);
+        rcChunkSetZ1(dst, src->verts[1].z);
+        rcChunkSetInvW1(dst, src->verts[1].inv_w);
+        rcChunkSetX2(dst, src->verts[2].x);
+        rcChunkSetY2(dst, src->verts[2].y);
+        rcChunkSetZ2(dst, src->verts[2].z);
+        rcChunkSetInvW2(dst, src->verts[2].inv_w);
+
+        if (src->type == CHUNK_COLORED) {
+            rcChunkSetColor(dst, src->colored.color);
+        } else {
+            rcChunkSetTexture(dst, src->textured.texture);
+            rcChunkSetUv0(dst, src->textured.uvs[0]);
+            rcChunkSetUv1(dst, src->textured.uvs[1]);
+            rcChunkSetUv2(dst, src->textured.uvs[2]);
+        }
+
+        ScreenAABB bb = chunk_screen_aabb(src);
+        rcScreenAABB bounds = rcScreenAABBAlloc();
+        rcScreenAABBSetXMin(bounds, bb.x_min);
+        rcScreenAABBSetXMax(bounds, bb.x_max);
+        rcScreenAABBSetYMin(bounds, bb.y_min);
+        rcScreenAABBSetYMax(bounds, bb.y_max);
+        rcChunkInsertBoundsScreenAABB(dst, bounds);
+
+        rcRootAppendChunk(root, dst);
+    }
+#else
+    (void)chunks;
+    (void)chunk_count;
+    (void)frame_index;
+#endif
+}
+
+void chunks_datadraw_stop(void) {
+#if CHUNKS_HAVE_DATADRAW
+    rcDatabaseStop();
+#endif
 }
---
+++ src/rctypedef.h
@@ -0,0 +1,8 @@
+#ifndef RCTYPEDEF_H
+#define RCTYPEDEF_H
+
+#include "chunk.h"
+
+typedef Texture *TexturePtr;
+
+#endif // RCTYPEDEF_H
